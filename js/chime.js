f0aaf62ce04579c21ca573ed54f6f1a33ce22703
Brandon Caruso <bcaruso@uw.edu>
2017-01-27 21:17:07 -0800

    Add show function and added drawChimeString() ---> showChimeString() which now uses Model data to draw strings

--- js/chime.js
+++ js/chime.js
@@ -20,17 +20,43 @@ function Chime(x_pos,y_pos,width,height){
           pointA: { x: this.constraintPos.x-(this.w/2)-10, y: this.constraintPos.y },
           pointB: { x: -1*(this.w/2)+10, y: -1*((this.h/2)-10) },
           stiffness: .8,
-          lenght: this.constraintLen
-  })
+          lenght: this.constraintLen });
 
   this.constraintRight = Constraint.create({
           bodyB: this.chime,
           pointA: { x: this.constraintPos.x+(this.w/2)+10, y: this.constraintPos.y },
           pointB: { x: (this.w/2)-10, y: -1*((this.h/2)-10) },
           stiffness: .8,
-          lenght: this.constraintLen
-  })
+          lenght: this.constraintLen });
 
   this.chimeModel = Composite.create();
-  Composite.add(this.chimeModel,[this.chime,this.constraintLeft,this.constraintRight])
+  Composite.add(this.chimeModel,[this.chime,this.constraintLeft,this.constraintRight]);
+
+  this.show = function(){
+      this.showChimeStrings();
+  }
+
+  this.showChimeStrings = function() {
+    push();
+    stroke('#45191B');
+    strokeWeight(3);
+    //Left Constraint
+    line(this.constraintLeft.pointA.x,
+         this.constraintLeft.pointA.y,
+         this.constraintLeft.bodyB.position.x +  this.constraintLeft.pointB.x,
+         this.constraintLeft.bodyB.position.y +  this.constraintLeft.pointB.y);
+    //Right Constraint
+    line(this.constraintRight.pointA.x,
+         this.constraintRight.pointA.y,
+         this.constraintRight.bodyB.position.x +  this.constraintRight.pointB.x,
+         this.constraintRight.bodyB.position.y +  this.constraintRight.pointB.y);
+    //String Around
+    line(this.constraintLeft.bodyB.position.x +  this.constraintLeft.pointB.x,
+         this.constraintLeft.bodyB.position.y +  this.constraintLeft.pointB.y,
+         this.constraintRight.bodyB.position.x +  this.constraintRight.pointB.x,
+         this.constraintRight.bodyB.position.y +  this.constraintRight.pointB.y);
+    pop();
+  }
+
+
 }
