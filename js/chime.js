c61d5c994e8a858f9f112bd9c4ef2754e2b866d1
Brandon Caruso <bcaruso@uw.edu>
2017-01-27 22:55:29 -0800

    Add showChimeBody() this gives the draw instructions for the body chime. Vertex values were generalized to be more flexible with changing width and height of chimes.

--- js/chime.js
+++ js/chime.js
@@ -6,7 +6,8 @@ function Chime(x_pos,y_pos,width,height){
   this.constraintLen = 100;
   this.w = width;
   this.h = height;
-  this.pos = createVector(this.constraintPos.x,this.constraintPos.y + this.h/2 + this.constraintLen-10);
+  this.pos = createVector(this.constraintPos.x,
+                          this.constraintPos.y + this.h/2 + this.constraintLen-10);
 
   this.constraintLeft;
   this.constraintRight;
@@ -17,15 +18,15 @@ function Chime(x_pos,y_pos,width,height){
 
   this.constraintLeft = Constraint.create({
           bodyB: this.chime,
-          pointA: { x: this.constraintPos.x-(this.w/2)-10, y: this.constraintPos.y },
-          pointB: { x: -1*(this.w/2)+10, y: -1*((this.h/2)-10) },
+          pointA: { x: this.constraintPos.x-(this.w/2)-20, y: this.constraintPos.y },
+          pointB: { x: -1*(this.w/2), y: -1*((this.h/2)-10) },
           stiffness: .8,
           lenght: this.constraintLen });
 
   this.constraintRight = Constraint.create({
           bodyB: this.chime,
-          pointA: { x: this.constraintPos.x+(this.w/2)+10, y: this.constraintPos.y },
-          pointB: { x: (this.w/2)-10, y: -1*((this.h/2)-10) },
+          pointA: { x: this.constraintPos.x+(this.w/2)+20, y: this.constraintPos.y },
+          pointB: { x: (this.w/2), y: -1*((this.h/2)-10) },
           stiffness: .8,
           lenght: this.constraintLen });
 
@@ -33,6 +34,7 @@ function Chime(x_pos,y_pos,width,height){
   Composite.add(this.chimeModel,[this.chime,this.constraintLeft,this.constraintRight]);
 
   this.show = function(){
+      this.showChimeBody();
       this.showChimeStrings();
   }
 
@@ -43,18 +45,81 @@ function Chime(x_pos,y_pos,width,height){
     //Left Constraint
     line(this.constraintLeft.pointA.x,
          this.constraintLeft.pointA.y,
-         this.constraintLeft.bodyB.position.x +  this.constraintLeft.pointB.x,
-         this.constraintLeft.bodyB.position.y +  this.constraintLeft.pointB.y);
+         this.constraintLeft.bodyB.position.x + this.constraintLeft.pointB.x,
+         this.constraintLeft.bodyB.position.y + this.constraintLeft.pointB.y);
     //Right Constraint
     line(this.constraintRight.pointA.x,
          this.constraintRight.pointA.y,
-         this.constraintRight.bodyB.position.x +  this.constraintRight.pointB.x,
-         this.constraintRight.bodyB.position.y +  this.constraintRight.pointB.y);
+         this.constraintRight.bodyB.position.x + this.constraintRight.pointB.x,
+         this.constraintRight.bodyB.position.y + this.constraintRight.pointB.y);
     //String Around
-    line(this.constraintLeft.bodyB.position.x +  this.constraintLeft.pointB.x,
-         this.constraintLeft.bodyB.position.y +  this.constraintLeft.pointB.y,
-         this.constraintRight.bodyB.position.x +  this.constraintRight.pointB.x,
-         this.constraintRight.bodyB.position.y +  this.constraintRight.pointB.y);
+    line(this.constraintLeft.bodyB.position.x + this.constraintLeft.pointB.x,
+         this.constraintLeft.bodyB.position.y + this.constraintLeft.pointB.y,
+         this.constraintRight.bodyB.position.x + this.constraintRight.pointB.x,
+         this.constraintRight.bodyB.position.y + this.constraintRight.pointB.y);
+    pop();
+  }
+
+  this.showChimeBody = function() {
+    push();
+
+    translate (this.chime.position.x,this.chime.position.y);
+    rotate(this.chime.angle)
+    translate (-this.w/2,-this.h/2); //LeftCorner of Chime
+
+    noStroke();
+    //basic chime body
+    fill("#CB6D23");
+    beginShape();
+    vertex (-10, -30);
+    vertex (this.w + 10, -30);
+    vertex (this.w + 10, 0);
+    vertex (this.w, 10);
+    vertex (this.w, this.h);
+    vertex ((this.w - this.w/2)+5, this.h + 30);
+    vertex ((this.w/2)-5, this.h + 30);
+    vertex (0, this.h);
+    vertex (0, 20);
+    vertex (0, 10);
+    vertex (-10, 0);
+    endShape(CLOSE);
+
+    //chime top
+    beginShape();
+    fill("#E8A774");
+    vertex (-10, -30);
+    vertex (this.w + 10, -30);
+    vertex (this.w + 10, 0);
+    vertex (-10, 0);
+    endShape(CLOSE);
+
+    //chime top shadow
+    beginShape();
+    fill("#854E2E");
+    vertex ( -10, 0);
+    vertex (this.w + 10, 0);
+    vertex (this.w , 10);
+    vertex (0, 10);
+    endShape(CLOSE);
+
+    // chime opening
+    beginShape();
+    fill('#541E21');
+    vertex (5, (this.h + 30)/2);
+    bezierVertex(15,
+                 (this.h + 30)/2 - 40,
+                 this.w -15,
+                 (this.h + 30)/2 - 40,
+                 this.w - 5,
+                 (this.h + 30)/2);
+    vertex (this.w - 5, this.h);
+    vertex ((this.w - this.w/2) + 2, this.h + 30);
+    vertex ((this.w/2) - 2, this.h + 30);
+    vertex (5, this.h);
+    stroke('#AA5719');
+    strokeWeight(5);
+    endShape(CLOSE);
+
     pop();
   }
 
