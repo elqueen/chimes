c03c2e8c355c2486ccfb4e0a5067d33bb637106f
Brandon Caruso <bcaruso@uw.edu>
2017-01-29 21:23:51 -0800

    Added temporary visual styling and adjust size and layout of pendulum

--- js/pendulum.js
+++ js/pendulum.js
@@ -1,12 +1,12 @@
 function Pendulum(x_pos,y_pos,width,height){
-    this.pos = createVector(x_pos,y_pos);
+    this.pos = createVector(x_pos,y_pos+chimeHangerThickness);
     this.w = width;
     this.h = height;
 
     this.pendulumModel = Composite.create();
 
-    this.pendulum = Bodies.circle(this.pos.x, this.pos.y + this.h*.4, this.w/2);
-    this.bottomDeco = Bodies.rectangle(this.pos.x, this.pos.y+this.h*.9, this.w*.75, this.w*.75);
+    this.pendulum = Bodies.circle(this.pos.x, this.pos.y + this.h*.35, this.w*.37);
+    this.bottomDeco = Bodies.rectangle(this.pos.x, this.pos.y+this.h*.8, this.w*.75, this.w*.75);
 
     this.constraintTop = Constraint.create({
             bodyB: this.pendulum,
@@ -25,6 +25,29 @@ function Pendulum(x_pos,y_pos,width,height){
                                        this.constraintBottom]);
 
     this.show = function(){
+        // NOTE: TEMPORARY
+        push();
+        stroke('#45191B');
+        strokeWeight(3);
+        // Draw Contraints
+        line(this.pos.x,this.pos.y-chimeHangerThickness,this.pos.x,this.pos.y);
+        line(this.pos.x,this.pos.y, this.pendulum.position.x,this.pendulum.position.y);
+        line(this.pendulum.position.x,this.pendulum.position.y,this.bottomDeco.position.x,this.bottomDeco.position.y);
+        noStroke();
+        //Draw pendulumn
+        fill('#541E21');
+        ellipseMode(CENTER);
+        ellipse(this.pendulum.position.x,this.pendulum.position.y,this.w*.75,this.w*.75);
 
+        //Draw Deco
+        push();
+        fill("#854E2E");
+        rectMode(CENTER);
+        push()
+        translate(this.bottomDeco.position.x,this.bottomDeco.position.y);
+        rotate(PI/4);
+        rect(0,0,this.w*.75, this.w*.75)
+        pop();
+        pop();
     }
 }
