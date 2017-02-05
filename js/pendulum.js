30846e1ea348cdb3723c6c09d3e02057725d097a
Brandon Caruso <bcaruso@uw.edu>
2017-02-05 01:22:34 -0800

    Fixed constraints and fixed drawing of constraints

--- js/pendulum.js
+++ js/pendulum.js
@@ -5,7 +5,7 @@ function Pendulum(x_pos,y_pos,width,height){
 
     this.pendulumModel = Composite.create();
 
-    this.pendulum = Bodies.circle(this.pos.x, this.pos.y + this.h*.35, this.w*.5);
+    this.pendulum = Bodies.circle(this.pos.x, this.pos.y + this.h*.3, this.w*.5,{restitution:.5});
     this.bottomDeco = Bodies.circle(this.pos.x,
                                        this.pos.y+this.h*.9,
                                        this.w, {frictionAir:.002});
@@ -14,43 +14,43 @@ function Pendulum(x_pos,y_pos,width,height){
     this.constraintTop = Constraint.create({
             bodyB: this.pendulum,
             pointA: { x: this.pos.x, y: this.pos.y },
-            stiffness: .1});
+            pointB: { x: 0, y: -this.w*.5 },
+            stiffness:.8});
 
     this.constraintBottom = Constraint.create({
             bodyA: this.pendulum,
             bodyB: this.bottomDeco,
-            stiffness: 1});
-    /*this.constraintInvisibleLeft = Constraint.create({
-            bodyA: this.pendulum,
-            bodyB: this.bottomDeco,
-            pointA: { x: 0, y: 200 },
-            pointB: { x: -10, y: -1*((sqrt(2*pow(this.w*1.5,2)))/2) + 10},
-            stiffness: .5});
-    this.constraintInvisibleRight = Constraint.create({
-            bodyA: this.pendulum,
-            bodyB: this.bottomDeco,
-            pointA: { x: 0, y: 200 },
-            pointB: { x: 10, y: -1*((sqrt(2*pow(this.w*1.5,2)))/2) + 10},
-            stiffness: .5});*/
+            pointA: { x: 0, y: this.w*.5},
+            pointB: { x: 0, y: -this.w},
+            stiffness: .8});
 
     Composite.add(this.pendulumModel,[this.pendulum,
                                        this.bottomDeco,
                                        this.constraintTop,
-                                       this.constraintBottom/*,
-                                       this.constraintInvisibleLeft,
-                                       this.constraintInvisibleRight*/]);
+                                       this.constraintBottom]);
 
     this.show = function(){
-        // NOTE: TEMPORARY
         push();
         stroke('#45191B');
         strokeWeight(3);
         // Draw Contraints
-        line(this.pos.x,this.pos.y-chimeHangerThickness,this.pos.x,this.pos.y);
-        line(this.pos.x,this.pos.y, this.pendulum.position.x,this.pendulum.position.y);
-        line(this.pendulum.position.x,this.pendulum.position.y,
-             this.bottomDeco.position.x,
-             this.bottomDeco.position.y - this.w);
+        line(this.pos.x-7,this.pos.y-chimeHangerThickness,
+             this.pos.x-5,this.pos.y);
+        line(this.pos.x,this.pos.y-chimeHangerThickness,
+             this.pos.x,this.pos.y);
+        line(this.pos.x+7,this.pos.y-chimeHangerThickness,
+             this.pos.x+5,this.pos.y);
+
+        line(this.constraintTop.pointA.x,
+             this.constraintTop.pointA.y,
+             this.constraintTop.bodyB.position.x + this.constraintTop.pointB.x,
+             this.constraintTop.bodyB.position.y + this.constraintTop.pointB.y);
+
+        line(this.constraintBottom.bodyA.position.x + this.constraintBottom.pointA.x,
+             this.constraintBottom.bodyA.position.y + this.constraintBottom.pointA.y,
+             this.constraintBottom.bodyB.position.x + this.constraintBottom.pointB.x,
+             this.constraintBottom.bodyB.position.y + this.constraintBottom.pointB.y);
+             
         pop();
 
         push();
@@ -66,9 +66,20 @@ function Pendulum(x_pos,y_pos,width,height){
           imageMode(CENTER);
             push();
             translate(this.bottomDeco.position.x,this.bottomDeco.position.y);
-              rotate(this.bottomDeco.angle);
-              image(imgDeco, 0, 0, this.w*2, this.w*2);
+            rotate(this.bottomDeco.angle);
+            image(imgDeco, 0, 0, this.w*2, this.w*2);
             pop();
+        pop();
+
+        push();
+          // Draw Little String Wrap around Deco
+          stroke('#45191B');
+          strokeWeight(5);
+          translate(this.bottomDeco.position.x,this.bottomDeco.position.y);
+          line(this.constraintBottom.pointB.x,
+               this.constraintBottom.pointB.y,
+               this.constraintBottom.pointB.x,
+               this.constraintBottom.pointB.y + 7);
         pop();
     }
 }
