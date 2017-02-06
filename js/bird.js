fd5d4a67afa8e48f1a74bb1d3f098f5323d2d55b
speedyqueenie <erica.queen8@gmail.com>
2017-02-06 11:13:37 -0800

    Adding the bird and rainbow js files.

---
+++ js/bird.js
@@ -0,0 +1,47 @@
+function Bird(){
+  this.x = windowWidth - 100;
+  this.y = 30;
+
+  this.show = function(){
+    noStroke();
+    // Feet
+    push();
+    beginShape();
+    stroke('#F2A844');
+    strokeWeight(2);
+    line(this.x+5, this.y+20, this.x, this.y);
+    line(this.x-5, this.y+20, this.x, this.y);
+    endShape();
+    pop();
+    // Body
+    beginShape();
+    fill('#8444B8');
+    ellipse(this.x+3, this.y, 25, 25);
+    endShape();
+    // Beak
+    beginShape();
+    fill('#F2A844');
+    triangle(this.x-30, this.y-15, this.x-20, this.y-10, this.x-20, this.y-20);
+    endShape();
+    // Head
+    beginShape();
+    fill('#8444B8');
+    ellipse(this.x-8, this.y-15, 25, 25);
+    endShape()
+    // Eye
+    beginShape();
+    fill(255,255,255)
+    ellipse(this.x-10, this.y-15, 15, 15);
+    fill(0, 0, 0);
+    ellipse(this.x-10, this.y-15, 10, 10);
+    fill(255,255,255);
+    ellipse(this.x-13, this.y-17, 3, 3);
+    endShape();
+    // Wing
+    //Tail
+    beginShape();
+    fill('#8444B8');
+    rect(this.x+10, this.y-15, 20, 20, 10, 5, 10, 5);
+    endShape()
+  }
+}
---
+++ js/rainbow.js
@@ -0,0 +1,44 @@
+function Rainbow() {
+    this.startAngle = PI;
+    this.endAngle = 0;
+    this.x = windowWidth/2;
+    this.y = windowHeight;
+    this.w = windowWidth*1.25;
+    this.h = windowHeight;
+    this.opacity;
+    var i; 
+
+    this.rainbowFadeIn = function() {
+      for (i = 0; i < 50; i++){
+        opacity = i/100;
+      }
+    };
+    this.fade = setInterval(this.rainbowFadeIn, 4000);
+    this.show = function(){
+        push();
+        beginShape();
+        colorMode(RGB, 255, 255, 255, 1);
+        noFill();
+        strokeWeight(100);
+        stroke(255, 0, 0, this.opacity);
+        arc(this.x, this.y, this.w, this.h, this.startAngle, this.endAngle);
+        stroke(250, 200, 0, this.opacity);
+        arc(this.x, this.y, this.w + 50, this.h + 50, this.startAngle, this.endAngle);
+        stroke(240, 255, 102, this.opacity);
+        arc(this.x, this.y, this.w + 100, this.h + 100, this.startAngle, this.endAngle);
+        stroke(29, 255, 13, this.opacity);
+        arc(this.x, this.y, this.w + 150, this.h + 150, this.startAngle, this.endAngle);
+        stroke(122, 155, 255, this.opacity);
+        arc(this.x, this.y, this.w + 200, this.h + 200, this.startAngle, this.endAngle);
+        endShape();
+        pop();
+    }
+    console.log(i);
+
+// function rainbowFadeOut(a) {
+//   for (a = 99; a--){
+//     a/100;
+//   }
+// }
+};
+
